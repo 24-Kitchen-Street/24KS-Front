30e4d669ed04715bd0a12d7c5152eb48f74eb925
Alex Kempton <alex@funwithtriangles.net>
2021-06-07 14:57:30 +0200

    Fake server: adding/removing players randomly

--- src/fakeServer.js
+++ src/fakeServer.js
@@ -4,29 +4,44 @@ import { Clock } from "three"
 import noise from "simplenoise"
 
 let players = []
-let numPlayers = 50
+let numPlayers = 100
+
+const chanceOfChange = 0.01
+const clock = new Clock()
+const range = 100
+
+const createPlayer = () => ({
+  id: Math.random() * 1000,
+  position: [0, 0, 0],
+  rotation: [0, 0, 0],
+})
 
 // Initialise a bunch of players
 for (let i = 0; i < numPlayers; i++) {
-  players.push({
-    id: Math.random() * 1000,
-    position: [0, 0, 0],
-    rotation: [0, 0, 0],
-  })
+  players.push(createPlayer())
 }
 
-const clock = new Clock()
-
 const update = () => {
+  // Maybe add a player
+  if (Math.random() < chanceOfChange) {
+    players.push(createPlayer())
+  }
+
+  // Maybe remove a player
+  if (Math.random() < chanceOfChange) {
+    const index = Math.floor(Math.random() * players.length)
+    players.splice(index, 1)
+  }
+
   players.forEach((p) => {
     noise.seed(p.id)
 
-    const t = clock.getElapsedTime() * 0.05
+    const t = clock.getElapsedTime() * 0.02
 
     p.position = [
-      noise.simplex2(p.id, t) * 30,
-      noise.simplex2(p.id + 1, t) * 30,
-      noise.simplex2(p.id + 2, t) * 30,
+      noise.simplex2(p.id, t) * range,
+      noise.simplex2(p.id + 1, t) * range,
+      noise.simplex2(p.id + 2, t) * range,
     ]
 
     p.rotation = [0, noise.simplex2(p.id, t) * Math.PI, 0]

c65887d059d87264a8bb617065306f3d746f74ad
Isabelle Bolt <izzybolt95@gmail.com>
2021-06-10 16:11:24 +0100

    updating empty file

---
+++ src/3d/ShaderSphere.js
@@ -0,0 +1,58 @@
+import React, { useRef, useFrame, useMemo } from "react";
+import * as THREE from "three";
+
+export const vertexShader = `
+      precision mediump float;
+      varying vec2 vUv;
+      void main() {
+          vec4 mvPosition = modelViewMatrix * vec4(position, 1.);
+          gl_Position = projectionMatrix * mvPosition;
+          vUv = uv;
+      }
+    `
+export const fragmentShader = `
+      varying vec2 vUv;
+      uniform float u_time;
+  
+      void main() {
+        vec2 uv = vUv;
+        float cb = floor((uv.x + u_time)*20.) + floor((uv.y + u_time)*20.);
+        gl_FragColor = vec4(0.,0.1,0.,mod(cb, 4.0));
+      }
+    `
+
+  
+ export function ShaderShape (props) {
+    const sphereRef = useRef();
+  
+
+    const uniforms = useMemo(
+        () => ({
+                u_time: { type: "f", value: 0 }
+        }),
+        []
+      )
+    
+    // why is useFrame not working??
+    //   useFrame(state => {
+    //     uniforms.time.value = state.clock.elapsedTime
+    //   })
+
+        // useFrame(({ clock }) => {
+    //   sphereRef.current.material.uniforms.u_time.value = clock.oldTime * 0.00005;
+    // });
+  
+    return (
+      <mesh ref={sphereRef} {...props}>
+        <sphereGeometry args={[2, 24, 24]} />
+        <shaderMaterial
+                attach="material"
+                uniforms={uniforms}
+                fragmentShader={fragmentShader}
+                vertexShader={vertexShader}
+                blending={THREE.AdditiveBlending}
+                transparent
+                vertexColors />
+      </mesh>
+    );
+  };
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const SOCKET_URI =
   process.env.REACT_APP_SOCKET_URI_MODE === "local"
-    ? "http://localhost:80"
+    ? "http://localhost:8080"
     : "https://back-24ks.herokuapp.com/"
 
 export const TICK_INTERVAL = 32

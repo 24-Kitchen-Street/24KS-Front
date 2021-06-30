1237b2cced809dbfca5521cc5fd596e4fd857b4a
Alex Kempton <alex@funwithtriangles.net>
2021-06-30 19:28:32 +0200

    Small perf change

--- src/3d/InstancedAvatars.js
+++ src/3d/InstancedAvatars.js
@@ -1,7 +1,7 @@
 // Adapted from https://codesandbox.io/s/instanced-vertex-colors-8fo01
 
 import * as THREE from "three"
-import React, { useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { useFrame } from "@react-three/fiber"
 import { useGLTF } from "@react-three/drei"
 import { WobblyMaterial } from "./WobblyMaterial"
@@ -21,22 +21,32 @@ let player
 export function InstancedAvatars({ materialConfig, players }) {
   const meshRef = useRef()
   const { nodes } = useGLTF("/avatar.glb")
+  const [shouldPurge, setShouldPurge] = useState(false)
+
+  useEffect(() => {
+    setShouldPurge(true)
+  }, [players.length])
 
   useFrame(({ clock }) => {
-    for (let i = 0; i < maxInstances; i++) {
+    // Only loop as much as we need
+    const count = shouldPurge ? maxInstances : players.length
+
+    for (let i = 0; i < count; i++) {
       player = players[i]
       if (player) {
         tempObject.position.set(...player.position)
         tempObject.rotation.set(...player.rotation)
         tempObject.updateMatrix()
         meshRef.current.setMatrixAt(i, tempObject.matrix)
-      } else {
+      } else if (shouldPurge) {
+        // If the number of players change, hide any loose instances
         meshRef.current.setMatrixAt(i, hiddenObject.matrix)
       }
     }
-    meshRef.current.instanceMatrix.needsUpdate = true
 
+    meshRef.current.instanceMatrix.needsUpdate = true
     materialConfig.time = clock.getElapsedTime()
+    setShouldPurge(false)
   })
   return (
     <instancedMesh

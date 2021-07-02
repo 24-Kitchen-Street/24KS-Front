3b2efaeab9aad5cb93d717e959e1aa9a36166f7f
Alex Kempton <alex@funwithtriangles.net>
2021-07-02 16:11:08 +0200

    AcidMaterial worked on

--- src/3d/Arena.js
+++ src/3d/Arena.js
@@ -1,35 +1,38 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useGLTF } from "@react-three/drei"
 import { streamMaterial } from "../utils/streamMaterial"
 import { AcidMaterial } from "./AcidMaterial"
 import { useFrame } from "@react-three/fiber"
 
+const acidMat = new AcidMaterial()
+
 export function Arena(props) {
   const { nodes, scene } = useGLTF("/arena2.glb")
 
-  const clubMat = nodes.structure.material
-  const clubMatScale = 10
-  clubMat.map.repeat.set(clubMatScale, clubMatScale)
-  clubMat.roughnessMap.repeat.set(clubMatScale, clubMatScale)
-  clubMat.roughness = 1
-  clubMat.metalness = 0
-  clubMat.normalMap.repeat.set(clubMatScale, clubMatScale)
-  clubMat.metalnessMap.repeat.set(clubMatScale, clubMatScale)
+  useEffect(() => {
+    const clubMat = nodes.structure.material
+    const clubMatScale = 10
+    clubMat.map.repeat.set(clubMatScale, clubMatScale)
+    clubMat.roughnessMap.repeat.set(clubMatScale, clubMatScale)
+    clubMat.roughness = 1
+    clubMat.metalness = 0
+    clubMat.normalMap.repeat.set(clubMatScale, clubMatScale)
+    clubMat.metalnessMap.repeat.set(clubMatScale, clubMatScale)
 
-  nodes.widescreen0.material = streamMaterial
-  nodes.widescreen1.material = streamMaterial
+    nodes.widescreen0.material = streamMaterial
+    nodes.widescreen1.material = streamMaterial
 
-  const acidMat = new AcidMaterial()
-  const numScreens = 19
-  for (let i = 0; i < numScreens; i++) {
-    nodes[`screen${i}`].material = acidMat
-  }
+    const numScreens = 19
+    for (let i = 0; i < numScreens; i++) {
+      nodes[`screen${i}`].material = acidMat
+    }
+  }, [nodes])
 
   useFrame(({ clock }) => {
     acidMat.time = clock.getElapsedTime()
   })
 
-  return <primitive object={scene} scale={50} />
+  return <primitive object={scene} scale={40} />
 }
 
 useGLTF.preload("/arena2.glb")
--- src/glsl/acid.js
+++ src/glsl/acid.js
@@ -1,10 +1,14 @@
 import glsl from "babel-plugin-glsl/macro"
 
 export const acid = glsl`
+#pragma glslify: snoise = require(glsl-noise-simplex/3d.glsl)
+
 uniform float time;
 uniform vec3 color;
 varying vec2 vUv;
 void main() {
-  gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + color, 1.0);
+
+  float p = fract(snoise(vec3(vUv.xy * 0.5, time * 0.5)) * 5.);
+  gl_FragColor.rgba = vec4(0.5 * sin(vUv.yxx + time) + color, p);
 }
 `

2eff019a034aad46843a7f2b99f87f1d1abfecaa
Alex Kempton <alex@funwithtriangles.net>
2021-07-09 15:15:51 +0200

    Cleaned up WobblyMaterial

--- src/3d/WobblyMaterial.js
+++ src/3d/WobblyMaterial.js
@@ -1,22 +1,29 @@
 /* eslint-disable react/jsx-pascal-case */
 
-import React from "react"
+import React, { useState } from "react"
 import { ShaderChunk } from "three"
 import ComponentMaterial from "component-material"
 import { verttop } from "../glsl/verttop.glsl.js"
 import { displaceBody } from "../glsl/displacebody.glsl.js"
+import { useFrame } from "@react-three/fiber"
 
-export const WobblyMaterial = ({ materialConfig, from }) => (
-  <ComponentMaterial
-    from={from}
-    uniforms={{
-      // speed: { value: materialConfig.speed, type: "float" },
-      time: { value: materialConfig.time, type: "float" },
-    }}
-    {...materialConfig}
-  >
-    <ComponentMaterial.Vert.Head>{
-      /* glsl */ `
+export const WobblyMaterial = ({ materialConfig, from }) => {
+  const [time, setTime] = useState(0)
+
+  useFrame(({ clock }) => {
+    setTime(clock.getElapsedTime())
+  })
+
+  return (
+    <ComponentMaterial
+      from={from}
+      uniforms={{
+        time: { value: time, type: "float" },
+      }}
+      {...materialConfig}
+    >
+      <ComponentMaterial.Vert.Head>{
+        /* glsl */ `
     ${verttop}
   
     vec3 displace(vec3 point) {
@@ -29,18 +36,19 @@ export const WobblyMaterial = ({ materialConfig, from }) => (
       return point;
     }
       `
-    }</ComponentMaterial.Vert.Head>
-    <ComponentMaterial.Vert.uv_vertex children={displaceBody} />
-    <ComponentMaterial.Vert.defaultnormal_vertex
-      replaceChunk
-      children={ShaderChunk.defaultnormal_vertex.replace(
-        "vec3 transformedNormal = objectNormal;",
-        `vec3 transformedNormal = displacedNormal;`
-      )}
-    />
-    <ComponentMaterial.Vert.displacementmap_vertex
-      replaceChunk
-      children="transformed = displacedPosition;"
-    />
-  </ComponentMaterial>
-)
+      }</ComponentMaterial.Vert.Head>
+      <ComponentMaterial.Vert.uv_vertex children={displaceBody} />
+      <ComponentMaterial.Vert.defaultnormal_vertex
+        replaceChunk
+        children={ShaderChunk.defaultnormal_vertex.replace(
+          "vec3 transformedNormal = objectNormal;",
+          `vec3 transformedNormal = displacedNormal;`
+        )}
+      />
+      <ComponentMaterial.Vert.displacementmap_vertex
+        replaceChunk
+        children="transformed = displacedPosition;"
+      />
+    </ComponentMaterial>
+  )
+}

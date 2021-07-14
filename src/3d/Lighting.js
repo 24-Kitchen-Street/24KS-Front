b72f6916fc0d4af274a0776c934137c2bf63866c
Alex Kempton <alex@funwithtriangles.net>
2021-07-14 12:50:52 +0200

    Brick room for character select

--- src/3d/Lighting.js
+++ src/3d/Lighting.js
@@ -20,34 +20,39 @@ export function Lighting() {
 
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime()
-    danceLight0.current.position.x = Math.sin(t) * 40
-    danceLight0.current.position.z = Math.cos(t * 2) * 30
-    dirLight.current.intensity = Math.sin(t) * 0.5
-    dirLight.current.target.position.x = Math.sin(t)
-    dirLight.current.target.position.y = Math.cos(t * 1.5)
-    dirLight.current.target.position.z = Math.cos(t)
+    if (me.isValid) {
+      danceLight0.current.position.x = Math.sin(t) * 40
+      danceLight0.current.position.z = Math.cos(t * 2) * 30
+      dirLight.current.intensity = Math.sin(t) * 0.5
+      dirLight.current.target.position.x = Math.sin(t)
+      dirLight.current.target.position.y = Math.cos(t * 1.5)
+      dirLight.current.target.position.z = Math.cos(t)
+    }
   })
   return (
     <>
-      {!me.isValid && <Environment preset="sunset" />}
       <directionalLight ref={dirLight} color={colorB} />
-      <group>
-        <pointLight distance={100} position={[0, 0, 500]} color={colorA} />
-        <pointLight distance={100} position={[0, 0, 200]} color={colorB} />
-        <pointLight
-          ref={danceLight0}
-          distance={100}
-          intensity={10}
-          color={colorA}
-        />
+      {!me.isValid ? (
+        <Environment preset="sunset" />
+      ) : (
+        <>
+          <pointLight distance={100} position={[0, 0, 500]} color={colorA} />
+          <pointLight distance={100} position={[0, 0, 200]} color={colorB} />
+          <pointLight
+            ref={danceLight0}
+            distance={100}
+            intensity={10}
+            color={colorA}
+          />
 
-        <pointLight
-          ref={danceLight0}
-          distance={100}
-          intensity={10}
-          color={colorB}
-        />
-      </group>
+          <pointLight
+            ref={danceLight0}
+            distance={100}
+            intensity={10}
+            color={colorB}
+          />
+        </>
+      )}
     </>
   )
 }
--- src/3d/World.js
+++ src/3d/World.js
@@ -2,13 +2,11 @@ import React from "react"
 import { Arena } from "./Arena"
 import { Avatars } from "./Avatars"
 import { Lighting } from "./Lighting"
-import { IntroRoom } from "./IntroRoom"
 
 export function World() {
   return (
     <>
       <Lighting />
-      <IntroRoom />
       <Avatars />
       <Arena />
     </>
